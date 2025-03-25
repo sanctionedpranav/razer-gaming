249dd654082ecad631bae57c7757d679893e7a12
sanctionedpranav <pranav9728@gmail.com>
2025-03-25 21:12:09 +0530

    hero section complete and about section is in progress

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <About />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnimatedTitle = ({title, containerClass}) => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Disc<b>o</b>ver the world&amp;s <br /> l<b>a</b>rgest shared adventure
+    </div>
+  );
+};
+
+export default AnimatedTitle;
