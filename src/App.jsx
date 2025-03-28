3c4ab12458364c39bd7d58693c9c4f62c144e6f6
sanctionedpranav <pranav9728@gmail.com>
2025-03-28 16:53:53 +0530

    build error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Story from "./components/Story";
-import Footer from ".components/Footer";
 
 function App() {
   return (
